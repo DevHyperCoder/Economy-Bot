228c00cdbbe680af3ce9c2a4919fc3050b7e43aa
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-11 14:21:26 +0530

    ✨ Add UserHelper.ts

---
+++ src/entityHelpers/UserHelper.ts
@@ -0,0 +1,22 @@
+import { User } from '../entities/User';
+
+export async function createUser(discordId: string, wallet?: number, bank?: number): Promise<User> {
+    const user = User.create()
+    user.discordId = discordId
+    user.wallet = wallet || 0
+    user.bank = bank ||0
+    return await user.save()
+}
+
+export async function checkIfUserExists(discordId: string): Promise<Boolean> {
+    const user = await User.findOne({discordId});
+    if (user) {
+        return true;
+    }
+    return false;
+}
+
+export async function getUser(discordId:string):Promise<User>{
+    const user = await User.findOne({discordId});
+    return user;
+}
