d40620114bf04c421e3c8667092868b3ae710ad5
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-11 14:44:30 +0530

    ♻️  Change to embed

--- src/commands/bank.ts
+++ src/commands/bank.ts
@@ -4,6 +4,8 @@ import { getUser } from '../entityHelpers/UserHelper'
 export default async function bank(msg: Message, args: string[]) {
     const user = (await getUser(msg.author.id));
     const embed = new MessageEmbed()
-    embed.setTitle(`${msg.author.id}'s Bank Balance`)
-    embed.addField("Bank Balance", user.bank)
+    embed.setTitle(`${msg.author.username}'s Bank Balance`)
+    embed.addFields({
+        name:"Bank Balance", value:user.bank})
+    msg.channel.send(embed)
 }
