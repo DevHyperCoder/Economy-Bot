e2fe022bfbf7efccf79705a4eba57c310ff717e2
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-11 14:55:35 +0530

    🎨  Refactor getRandInteger to sperate file ✨ Add wallet feature

--- src/commands/beg.ts
+++ src/commands/beg.ts
@@ -1,24 +1,22 @@
 import { Message } from "discord.js";
 import { getUser } from '../entityHelpers/UserHelper'
-import bank from './bank'
+import wallet from "./wallet";
+
+import getRandInteger from '../utils/randInt'
 
 // Beg
 export default async function beg(msg: Message, args: string[]) {
-// Get random number
-    const random = getRandInteger(900,1000);
+    // Get random number
+    const random = getRandInteger(900, 1000);
 
     // get msg id 
     const discordId = msg.author.id
 
     // get the user and update
     const user = await getUser(discordId)
-    user.bank += random;
+    user.wallet += random;
     await user.save()
 
     // dispay the balance
-    bank(msg, args);
+    wallet(msg, args);
 }
-
-function getRandInteger(min:number, max:number) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
--- src/commands/wallet.ts
+++ src/commands/wallet.ts
@@ -1,5 +1,15 @@
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
+import { getUser } from "..//entityHelpers/UserHelper";
 
 export default async  function wallet(msg:Message,args:string[]){
-    msg.reply(args);
+    const discordId = msg.author.id
+
+    const user = await getUser(discordId);
+    
+    const embed = new MessageEmbed();
+    embed.setTitle(`${msg.author.username}'s Wallet`)
+    embed.addField("Wallet",user.wallet)
+
+    msg.reply(embed)
+
 }
